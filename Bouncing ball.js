03a85fcf8c75baf28bb185ccd171f4bd40d99e95
ErikRice <erik.m.rice12@gmail.com>
2021-03-30 18:33:36 -0400

    changed Html and css files

--- Bouncing ball.js
+++ Bouncing ball.js
@@ -1,29 +1,17 @@
-<div id="ball" style="
-  z-index:5;
-  position:absolute;
-  left:0px;
-  top:0px;
-  width:50px;
-  height:50px;
-  border-radius:50%;
-  background:rgb(128, 0, 28)";></div>
 
-
-
-
-var velocityX = 7;
+const ball = document.getElementById("ball");
+var velocityX = 5;
 var velocityY = 3;
 var positionX = 0;
 var positionY = 0;
-var ball = document.getElementById('ball');
-const ballMovesRight = true;
-const ballMovesDown = true;
+var ballMovesRight = true;
+var ballMovesDown = true;
 
 
 function moveBall(){
   if (ballMovesRight) {
     positionX = positionX + velocityX;
-    ball.style.left = positionX + 'px';
+    ball.style.left = positionX;
     } else if (!ballMovesRight){
         positionX = positionX - velocityX;
         ball.style.left = positionX + 'px';
@@ -42,6 +30,4 @@ if (positionY >= 790 || positionY == 0){
   ballMovesDown = !ballMovesDown;
 }
 }
-
-
 setInterval(moveBall,8);
